1ed518c80086b13eeb9764dbc3c05eb248f556e8
Abdulrazaq Alhendi <dev@aalhendi.com>
2026-05-07 10:48:34 +0300

    Polish EngineSound_VolumeAdjust decomp

--- decompile/WorkInProgress/src/HOWL/h135_EngineSound_VolumeAdjust.c
+++ decompile/WorkInProgress/src/HOWL/h135_EngineSound_VolumeAdjust.c
@@ -1,6 +1,6 @@
 #include <common.h>
 
-int DECOMP_EngineSound_VolumeAdjust(int desired, int current, int amount)
+int EngineSound_VolumeAdjust(int desired, int current, int amount)
 {
   int gap = desired - current;
   int absGap = (gap < 0) ? -gap : gap;
@@ -8,7 +8,7 @@ int DECOMP_EngineSound_VolumeAdjust(int desired, int current, int amount)
   // amount less than absolute gap between
   // "current" and "desired"
   if (amount < absGap) {
-    return (gap < 0) ? current - amount : current + amount;
+    return (gap < 1) ? current - amount : current + amount;
   }
 
   return desired;
