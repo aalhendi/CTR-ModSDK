2a7985d23c7f9bece9a92b95287529ef293edb89
Abdulrazaq Alhendi <dev@aalhendi.com>
2026-05-07 10:47:02 +0300

    Polish PlayWarppadSound decomp

--- decompile/WorkInProgress/src/HOWL/h127_PlayWarppadSound.c
+++ decompile/WorkInProgress/src/HOWL/h127_PlayWarppadSound.c
@@ -1,4 +1,8 @@
+#include <common.h>
+
+void CalculateVolumeFromDistance(u_int *soundCount_ID, u_int soundID, int distance);
+
 void PlayWarppadSound(u_int distance)
 {
-    CalculateVolumeFromDistance(&sdata->soundFadeInput[0].soundID_soundCount, 152, distance);
+    CalculateVolumeFromDistance(&sdata_static.SoundFadeInput[0].soundID_soundCount, 152, distance);
 }
