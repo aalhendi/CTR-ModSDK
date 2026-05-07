9e3e1b230551ee702c0330f0722bb60f4f0cab08
Abdulrazaq Alhendi <dev@aalhendi.com>
2026-05-07 11:49:41 +0300

    Split DecalGlobal find helpers

--- decompile/WorkInProgress/src/DecalGlobal_Find.c
+++ decompile/WorkInProgress/src/DecalGlobal_Find.c
@@ -38,30 +38,3 @@ struct IconGroup * DECOMP_DecalGlobal_FindInLEV(struct Level *lev, int *checkByt
   }
   return NULL;
 }
-
-int * DECOMP_DecalGlobal_FindInMPK(int* curr, int* check)
-{
-  while (*(char *)curr != 0)
-  {
-    // check first 16 bytes,
-    // remember this is an int array, not char
-    if (((
-        // first character
-        (curr[0] == check[0]) &&
-
-        // second character
-        (curr[1] == check[1])) &&
-
-        // third character 
-        (curr[2] == check[2])) &&
-
-        // fourth
-        (curr[3] == check[3]))
-    {
-      // found the texture
-      return curr;
-    }
-    curr += 8;
-  }
-  return NULL;
-}
