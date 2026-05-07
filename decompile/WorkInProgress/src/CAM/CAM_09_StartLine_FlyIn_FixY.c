f703593ac45cab3461145540ab046fe03fbbd546
Abdulrazaq Alhendi <dev@aalhendi.com>
2026-05-07 11:35:40 +0300

    Polish CAM_StartLine_FlyIn_FixY decomp

--- decompile/WorkInProgress/src/CAM/CAM_09_StartLine_FlyIn_FixY.c
+++ decompile/WorkInProgress/src/CAM/CAM_09_StartLine_FlyIn_FixY.c
@@ -1,17 +1,14 @@
 #include <common.h>
 
-void DECOMP_CAM_StartLine_FlyIn_FixY(short* posRot) 
+u_char* CAM_StartLine_FlyIn_FixY(u_short* posRot)
 {
   int iVar1;
   int iVar2;
-  short local_26;
-  short sVar3;
-  short local_20;
-  short local_1e;
-  short local_1c;
-  short local_18;
-  short local_16;
-  short local_14;
+  short posTop[3];
+  short posBottom[3];
+  short posX;
+  short posY;
+  short posZ;
 
   // lev -> mesh_info
   sdata->scratchpadStruct.ptr_mesh_info = sdata->gGT->level1->ptr_mesh_info;
@@ -19,24 +16,36 @@ void DECOMP_CAM_StartLine_FlyIn_FixY(short* posRot)
   sdata->scratchpadStruct.Union.QuadBlockColl.qbFlagsWanted = 0x3000; // ground and wall
   sdata->scratchpadStruct.Union.QuadBlockColl.qbFlagsIgnored = 0;
 
-  local_20 = posRot[0];
-  local_26 = posRot[1];
-  local_1c = posRot[2];
+  posX = posRot[0];
+  posY = posRot[1];
+  posZ = posRot[2];
   
   for (iVar2 = 0; iVar2 < 8; iVar2++)
   {
     iVar1 = 0x400 * iVar2;
-    local_1e = local_26 - (iVar1 + 0x400);
-    local_16 = local_26 - (iVar1 - 0x100);
-    local_18 = local_20;
-    local_14 = local_1c;
+    posTop[0] = posX;
+    posTop[1] = posY - (iVar1 + 0x400);
+    posTop[2] = posZ;
+    posBottom[0] = posX;
+    posBottom[1] = posY - (iVar1 - 0x100);
+    posBottom[2] = posZ;
 
-    COLL_SearchBSP_CallbackQUADBLK(&local_20,&local_18,&sdata->scratchpadStruct,0,local_20,local_1c);
+    COLL_SearchBSP_CallbackQUADBLK(
+        (u_int*)posTop,
+        (u_int*)posBottom,
+        &sdata->scratchpadStruct,
+        0);
 
-    sVar3 = sdata->scratchpadStruct.Union.QuadBlockColl.unk1E;
-    if (sdata->scratchpadStruct.boolDidTouchQuadblock != 0) break;
-    sVar3 = local_26;
+    if (sdata->scratchpadStruct.boolDidTouchQuadblock != 0)
+    {
+      posX = sdata->scratchpadStruct.Union.QuadBlockColl.hitPos[0];
+      posY = sdata->scratchpadStruct.Union.QuadBlockColl.hitPos[1];
+      posZ = sdata->scratchpadStruct.Union.QuadBlockColl.hitPos[2];
+      break;
+    }
   }
-  
-  posRot[1] = sVar3;
+
+  posRot[1] = posY;
+
+  return (u_char*)&sdata->scratchpadStruct;
 }
