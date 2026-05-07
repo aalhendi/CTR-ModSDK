380a28ae251758bf35b6a5f0539f44b3fe8fc8c8
Abdulrazaq Alhendi <dev@aalhendi.com>
2026-05-07 10:53:17 +0300

    Polish RB_Banner_ThTick decomp

--- decompile/WorkInProgress/src/231/RB_Banner_ThTick.c
+++ decompile/WorkInProgress/src/231/RB_Banner_ThTick.c
@@ -1,12 +1,14 @@
 #include <common.h>
 
+void RB_Banner_Animate_Play(struct ModelHeader* headers, short param_2);
+
 void RB_Banner_ThTick(struct Thread* t)
 {
   struct StartBanner *banner = t->object;
 
   // Basically checks if banner should wave,
   // which does not happen in 3P or 4P multiplayer
-  if (banner->unk1 != NULL)
+  if (banner->unk1 != 0)
   {
     RB_Banner_Animate_Play(
         t->inst->model->headers,
