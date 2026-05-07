a164fa3e4b1e09fd653828d3943d39c916fcc0cc
Abdulrazaq Alhendi <dev@aalhendi.com>
2026-05-07 11:09:47 +0300

    Polish SelectProfile_ToggleMode decomp

--- decompile/WorkInProgress/src/SelectProfile/SelectProfile_ToggleMode.c
+++ decompile/WorkInProgress/src/SelectProfile/SelectProfile_ToggleMode.c
@@ -1,27 +1,29 @@
 #include <common.h>
 
-void SelectProfile_ToggleMode(int mode)
+void SelectProfile_ToggleMode(u_int mode)
 {
-  sdata->memcardAction = mode & 0xf;
+  short *modeData = (short*)sdata->data10_bbb;
+
+  *(short*)&sdata->memcardAction = mode & 0xf;
 
   // 0x00 - AdvNew (garage) mode
   // 0x10 - AdvLoad (main menu) mode
   // 0x20 - Green mode
   // 0x30 - Ghost mode
   // 0x40 - SlotSelected (exiting) mode
-  *(int*)&sdata->data10_bbb[0] = mode & 0xf0;
+  modeData[0] = mode & 0xf0;
 
-  *(short*)&sdata->data10_bbb[4] = 0;
-  *(short*)&sdata->data10_bbb[6] = 0;
-  *(short*)&sdata->data10_bbb[8] = 0;
-  *(short*)&sdata->data10_bbb[10] = 0;
-  *(short*)&sdata->data10_bbb[12] = 0;
+  modeData[2] = 0;
+  modeData[3] = 0;
+  modeData[4] = 0;
+  modeData[5] = 0;
+  modeData[6] = 0;
 
   SelectProfile_UnMuteCursors();
 
   data.menuFourAdvProfiles.drawStyle = data.menuFourAdvProfiles.drawStyle & 0xffef;
   data.menuOverwriteAdv.drawStyle &= 0xffef;
-  if (sdata->data10_bbb[0] == 0x20) {
+  if (modeData[0] == 0x20) {
     data.menuFourAdvProfiles.drawStyle = data.menuFourAdvProfiles.drawStyle | 0x10;
     data.menuOverwriteAdv.drawStyle |= 0x10;
   }
@@ -30,5 +32,5 @@ void SelectProfile_ToggleMode(int mode)
   SelectProfile_Init(data.menuFourAdvProfiles.drawStyle);
 
   data.menuFourAdvProfiles.rowSelected = sdata->unk_8008d73C_relatedToRowHighlighted;
-  *(short*)&sdata->data10_bbb = 0;
+  modeData[1] = 0;
 }
