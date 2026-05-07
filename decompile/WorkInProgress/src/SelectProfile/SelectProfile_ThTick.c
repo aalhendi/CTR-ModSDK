b635ab06d97070e26f0bfdea19e2e98db87a0c69
Abdulrazaq Alhendi <dev@aalhendi.com>
2026-05-07 11:05:27 +0300

    Polish SelectProfile_ThTick decomp

--- decompile/WorkInProgress/src/SelectProfile/SelectProfile_ThTick.c
+++ decompile/WorkInProgress/src/SelectProfile/SelectProfile_ThTick.c
@@ -1,7 +1,23 @@
 #include <common.h>
 
+struct SelectProfile_LoadSaveData
+{
+  struct Instance *inst;
+  short rot[3];
+  short padding;
+};
+
+struct SelectProfile_LoadSaveObj
+{
+  struct Thread *thread;
+  struct SelectProfile_LoadSaveData *loadSaveData;
+};
+
 void SelectProfile_ThTick(struct Thread *t)
 {
+  struct SelectProfile_LoadSaveData *loadSaveData =
+    ((struct SelectProfile_LoadSaveObj*)t->object)->loadSaveData;
+
   // loop through 12 instances,
   // 3 instances per profile, 4 profile slots
   for (int i = 0; i < 12; i++)
@@ -10,18 +26,21 @@ void SelectProfile_ThTick(struct Thread *t)
     int objIndex = (i % 3);
 
     // and add its corresponding rate
-    sdata->LoadSaveData[i].rot[1] += sdata->LoadSave_SpinRateY[objIndex];
+    loadSaveData[i].rot[1] += sdata->LoadSave_SpinRateY[objIndex];
 
-    struct Instance * inst = sdata->LoadSaveData[i].inst;
+    struct Instance * inst = loadSaveData[i].inst;
 
     // convert 3 rotation shorts into rotation matrix
-    ConvertRotToMatrix(inst->matrix, sdata->LoadSaveData[i].rot);
+    ConvertRotToMatrix(&inst->matrix, loadSaveData[i].rot);
 
     if (objIndex != 1)
     {
       // from MetaDataSaveLoad
-      short *rot = data.MetaDataLoadSave[i]->vec3_specular_inverted;
-      Vector_SpecLightSpin3D(inst->matrix, sdata->LoadSaveData[i].rot, rot);
+      short rot[3];
+      rot[0] = data.MetaDataLoadSave[i].vec3_specular_inverted[0];
+      rot[1] = data.MetaDataLoadSave[i].vec3_specular_inverted[1];
+      rot[2] = data.MetaDataLoadSave[i].vec3_specular_inverted[2];
+      Vector_SpecLightSpin3D(inst, loadSaveData[i].rot, rot);
     }
   }
 }
