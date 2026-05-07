a34bb0e57d3036e2441e75dbfe41e1ddcc23c0f1
Abdulrazaq Alhendi <dev@aalhendi.com>
2026-05-07 11:50:39 +0300

    Polish DecalGlobal_FindInLEV decomp

--- decompile/WorkInProgress/src/DecalGlobal_Find.c
+++ decompile/WorkInProgress/src/DecalGlobal_Find.c
@@ -1,39 +1,41 @@
 #include <common.h>
 
-struct IconGroup * DECOMP_DecalGlobal_FindInLEV(struct Level *lev, int *checkBytes)
+int * DecalGlobal_FindInLEV(struct Level *lev, char *str)
 {
   struct LevTexLookup *lookup;
-  struct IconGroup *curr;
-  struct IconGroup **first;
+  struct IconGroup **curr;
   struct IconGroup **last;
+  int *checkBytes;
+  int *iconGroup;
 
   // lev -> levTexLookup
   lookup = lev->levTexLookup;
+  checkBytes = (int *)str;
 
   // if pointer is valid
   if (lookup != NULL)
   {
     // pointer to first
-    first = lookup->firstIconGroupPtr;
+    curr = lookup->firstIconGroupPtr;
 
     // pointer to end
-    last = first[lookup->numIconGroup];
-    while ((int)first < (int)last);
+    last = curr + lookup->numIconGroup;
+    while (curr < last)
     {
-      curr = first[0].name;
+      iconGroup = (int *)*curr;
 
       // check first 16 bytes,
       // remember this is an int array, not char
-      if ((((curr[0] == checkBytes[0]) &&
-            (curr[1] == checkBytes[1])) &&
-            (curr[2] == checkBytes[2])) &&
-            (curr[3] == checkBytes[3]))
+      if ((((iconGroup[0] == checkBytes[0]) &&
+            (iconGroup[1] == checkBytes[1])) &&
+            (iconGroup[2] == checkBytes[2])) &&
+            (iconGroup[3] == checkBytes[3]))
       {
         // return pointer
-        return curr;
+        return iconGroup;
       }
       // next in loop
-      first = first + 1;
+      curr++;
     } 
   }
   return NULL;
