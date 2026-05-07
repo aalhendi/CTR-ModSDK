611ccea670434d936f0c2e9de72c31dbdabcb4f0
Abdulrazaq Alhendi <dev@aalhendi.com>
2026-05-07 11:21:46 +0300

    Polish UI_CupStandings_FinalizeCupRanks decomp

--- decompile/WorkInProgress/src/UI/UI_CupStandings_FinalizeCupRanks.c
+++ decompile/WorkInProgress/src/UI/UI_CupStandings_FinalizeCupRanks.c
@@ -1,15 +1,17 @@
 #include <common.h>
 
-void DECOMP_UI_CupStandings_FinalizeCupRanks(void)
+void UI_CupStandings_FinalizeCupRanks(void)
 {
   struct GameTracker *gGT;
   int totalNumCharacters;
   int trackRank;
   int i, j;
-  short numEqualRanks, rank;
+  short numEqualRanks;
+  short bestRank;
   int numRanks, rankDriver;
 
   gGT = sdata->gGT;
+  numEqualRanks = 0;
 
   // numPlyrCurrGame + number of AIs
   totalNumCharacters = gGT->numPlyrCurrGame + gGT->numBotsNextGame;
@@ -21,40 +23,38 @@ void DECOMP_UI_CupStandings_FinalizeCupRanks(void)
 
   // if there is more than one driver
   if (1 < totalNumCharacters) {
-    numEqualRanks = 0;
     for (i = 1; i < totalNumCharacters;i++)
     {
 	  // if current player cup points is the same as another player
       if ((sdata->gGT->cup.points[data.cupPositionPerPlayer[i]]) !=
-          (gGT->cup.points[data.cupPositionPerPlayer[1]])) break;
+          (gGT->cup.points[data.cupPositionPerPlayer[0]])) break;
       // add the number of driver with same rank
       numEqualRanks++;
     } 
   }
 
-  if (numEqualRanks > 0) {
-    for (i = 0; i < numEqualRanks; i++)
-    {
+  for (i = 0; i < numEqualRanks + 1; i++)
+  {
     // default rank at the most behind
-      rank = 99;
-      for(j = i; j < numEqualRanks; j++)
+      bestRank = 99;
+      numRanks = -1;
+      for(j = i; j < numEqualRanks + 1; j++)
 	  {
 		// get current driver rank for this track
         trackRank = gGT->drivers[data.cupPositionPerPlayer[j]]->driverRank;
         // if current driver rank is better (lower) than 99
-        if (trackRank < rank)
+        if (trackRank < bestRank)
 		{
         // save rank of current driver
-          rank = trackRank;
+          bestRank = trackRank;
           numRanks = j;
         }
       }
       // get driverID from cup rank i
       rankDriver = data.cupPositionPerPlayer[i];
 	  // set final cup rank for each driver
+      data.cupPositionPerPlayer[i] = numRanks;
       data.cupPositionPerPlayer[numRanks] = rankDriver;
-    }
   }
   return;
 }
-
