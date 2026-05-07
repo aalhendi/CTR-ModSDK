90068169b7b0861088e5dc03a2ea6aea49512e33
Abdulrazaq Alhendi <dev@aalhendi.com>
2026-05-07 11:12:40 +0300

    Polish CTR_ClearRenderLists_3P4P decomp

--- decompile/WorkInProgress/src/CTR/CTR_ClearRenderLists_3P4P.c
+++ decompile/WorkInProgress/src/CTR/CTR_ClearRenderLists_3P4P.c
@@ -1,17 +1,17 @@
 #include <common.h>
 
-void DECOMP_CTR_ClearRenderLists_3P4P(struct GameTracker* gGT, char numPlyrCurrGame)
+void CTR_ClearRenderLists_3P4P(struct GameTracker* gGT, int numPlyrCurrGame)
 {
   void **quads;
 
-  if (!numPlyrCurrGame) return;
+  if (numPlyrCurrGame <= 0) return;
   
 	// pointers to rendered quadblocks
     quads = &data.ptrRenderedQuadblockDestination_again[0];
 	
 	// for each player, clear render lists,
 	// set default destination for rendered quadblocks
-    for (char i = 0; i < numPlyrCurrGame; i++)
+    for (int i = 0; i < numPlyrCurrGame; i++)
 	  {
       gGT->LevRenderLists[i].list[0].bspListStart = 0;
       gGT->LevRenderLists[i].list[0].ptrQuadBlocksRendered = quads[i];
@@ -24,7 +24,7 @@ void DECOMP_CTR_ClearRenderLists_3P4P(struct GameTracker* gGT, char numPlyrCurrG
 	  // different from prev function, cause 
 	  // the RenderLists are in a different order 
 	  // in 1P2P and 3P4P
-      gGT->LevRenderLists[i].list[4].ptrQuadBlocksRendered = quads[i];
+      gGT->LevRenderLists[i].list[4].ptrQuadBlocksRendered = 0;
       gGT->LevRenderLists[i].list[3].ptrQuadBlocksRendered = quads[i];
     } 
 }
